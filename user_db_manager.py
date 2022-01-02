748589b572007194718dc82e3521cfeff3bb1102
GreenFish <fish@fish35.ml>
2022-01-02 10:20:16 +0530

    dummy code for user_db_manager.py

--- user_db_manager.py
+++ user_db_manager.py
@@ -2,28 +2,31 @@ import plyvel
 import json
 
 def user_db_get(key):
-    db = plyvel.DB('user_db', create_if_missing=True)
+    db = plyvel.DB('data/user_db', create_if_missing=True)
     data = db.get(key.encode())
     if data == None:
-        return False
+        return {}
     data = json.loads(data.decode())
     db.close()
     return data
 
 def user_db_put(key, value):
-    db = plyvel.DB('user_db', create_if_missing=True)
+    db = plyvel.DB('data/user_db', create_if_missing=True)
     db.put(key.encode(), json.dumps(value).encode())
     db.close()
 
 def user_db_del(key):
-    db = plyvel.DB('user_db', create_if_missing=True)
+    db = plyvel.DB('data/user_db', create_if_missing=True)
     db.delete(key.encode())
     db.close()
 
 def user_db_list():
-    db = plyvel.DB('user_db', create_if_missing=True)
+    db = plyvel.DB('data/user_db', create_if_missing=True)
     data = []
     for key, _ in db:
         data.append(key.decode())
     db.close()
     return data
+
+if __name__ == '__main__':
+    print(user_db_list())
