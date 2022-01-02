c1b81efb82ce938a4b167eb4d3aa3c62679473d1
GreenFish <fish@fish35.ml>
2022-01-02 10:21:24 +0530

    renamed auth_manager.py

--- auth_manager.py
+++ auth_manager.py
@@ -1,7 +1,23 @@
+"""
+# Authentication Database manager.
+# Holds only double-hashed passwords of users.
+
+Authentication Manager.
+Passwordless login with Google.
+Handles JWT tokens.
+"""
+
 import json
-import index_db_manager
+import session_db_manager
 import hashlib
 import base64
+from google.oauth2 import id_token
+from google.auth.transport import requests
+import user_db_indexer
+import re
+
+CLIENT_ID = "18178721920-v27esm525ddg2pdmemu7i37olq9bglkq.apps.googleusercontent.com"
+authorized_email_domains = ["@gmail.com", "@diyaschool.com"]
 
 def read_user_db(username):
     with open('data/auth.db') as f:
@@ -15,10 +31,20 @@ def read_user_db(username):
 def verify_creds(username, password):
     user_data = read_user_db(username)
     if user_data == False:
-        return (False, "Username not found")
+        return (False, "ACCOUNT_NOT_FOUND")
     if user_data['password'] == base64.b64encode(hashlib.sha256(hashlib.sha256(password.encode()).digest()).digest()).decode():
-        return (True, "Authentication successful")
-    return (False, "Password incorrect")
+        return (True, "AUTH_SUCCESS")
+    return (False, "PASSWORD_INCORRECT")
+
+def authorize_jwt(jwt_token):
+    idinfo = id_token.verify_oauth2_token(jwt_token, requests.Request(), CLIENT_ID)
+    email = idinfo['email']
+    if re.search("@[\w.]+", email).group() not in authorized_email_domains:
+        return False, "EXTERNAL_ACCOUNT", None, None
+    user_data = user_db_indexer.get_by_email(email)
+    if user_data == False:
+        return False, "ACCOUNT_NOT_FOUND", None, None
+    return True, None, user_data['username'], email
 
 def write(username, data):
     with open('data/auth.db') as f:
@@ -27,28 +53,20 @@ def write(username, data):
     with open('data/auth.db', 'w') as f:
         f.write(json.dumps(all_data))
 
-def modify_user(username, password, name, email, tags):
+def modify_user(username, password):
     data = read_user_db(username)
     if data == False:
         return False
     if password != None:
         password = base64.b64encode(hashlib.sha256(hashlib.sha256(password.encode()).digest()).digest()).decode()
         data['password'] = password
-    if email != None:
-        data['email'] = email
-    if tags != None:
-        data['tags'] = tags
-    if name != None:
-        data['name'] = name
     write(username, data)
 
-def new_user(username, password, name, email, tags):
-    if index_db_manager.get_by_username(username) != False:
+def new_user(username, password):
+    if session_db_manager.get_by_username(username) != False:
         return (False, "Username already exists")
-    if index_db_manager.get_by_email(email) != False:
-        return (False, "Email already exists")
     password = base64.b64encode(hashlib.sha256(hashlib.sha256(password.encode()).digest()).digest()).decode()
-    user_data = {'password': password, 'name': name, 'email': email, 'tags': tags}
+    user_data = {'password': password}
     write(username, user_data)
     return (True, "Written successfully")
 
@@ -62,4 +80,4 @@ def del_user(username):
         f.write(json.dumps(data))
 
 if __name__ == '__main__':
-    del_user('test')
+    print(read_user_db())
