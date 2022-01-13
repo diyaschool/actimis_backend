0e5dd18991feacef1b7444ac865ae3101f2b45da
GreenFish <fish@fish35.ml>
2022-01-14 03:39:12 +0530

    added more functions for user_session in test_manager

--- test_manager.py
+++ test_manager.py
@@ -2,13 +2,28 @@ import json
 
 class classic_mcq:
     def get_test_metadata(test_id):
-        data_path="data/test_db/classic_mcq/"
-        with open(data_path+'test_data/metadata.json') as f:
-            data = json.loads(f.read())
+        try:
+            with open("data/test_db/classic_mcq/test_data/metadata.json") as f:
+                data = json.loads(f.read())
+        except FileNotFoundError:
+            return None
         try:
             test_metadata = data[test_id]
             return test_metadata
         except KeyError:
+            return False
+    def get_user_session(session_id):
+        try:
+            with open(f"data/test_db/classic_mcq/user_sessions/{session_id}.json") as f:
+                data = json.loads(f.read())
+        except FileNotFoundError:
+            return False
+        return data
+    def write_user_session(session_id, data):
+        try:
+            with open(f"data/test_db/classic_mcq/user_sessions/{session_id}.json", "w") as f:
+                f.write(json.dumps(data))
+        except FileNotFoundError:
             return False
 
 if __name__ == '__main__':
